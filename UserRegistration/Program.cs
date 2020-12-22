d05d77e19f8590a974f039bb0060281aa0dd44a2
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-12-22 10:56:40 +0530

    [Hemalatha] Add:Implementation of User First Name start with Caps and has minimum 3 Characters

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration program");
+            Patterns PatternsMatch = new Patterns();
+            bool value = PatternsMatch.ValidateFirstName("aema");
+            Console.WriteLine(value);
+        }
+    }
+}
