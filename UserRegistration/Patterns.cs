ba161dc48c7f1dc0f7f8027ed5c2dedc6db44bcd
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-12-22 22:43:51 +0530

    [Hemalatha] Refactor:Implementation of User Password with minimum 8 Characters

--- UserRegistration/Patterns.cs
+++ UserRegistration/Patterns.cs
@@ -10,7 +10,8 @@ namespace UserRegistration
       public static string REGEX_FIRSTNAME = "^[A-Z]{1}[a-z]{2,}";
       public static string REGEX_LASTNAME = "[A-Z]{1}[a-z]{2,}";
       public static string REGEX_EMAILID = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}";
-        public static string REGEX_MOBILENUMBER = "[0-9]{2}[ ]?[0-9]{10}$";
+      public static string REGEX_MOBILENUMBER = "[0-9]{2}[ ]?[0-9]{10}$";
+      public static string REGEX_PASSWORD = "[A-Za-z0-9]{8,}";
         public bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, REGEX_FIRSTNAME);  
@@ -27,6 +28,11 @@ namespace UserRegistration
         {
             return Regex.IsMatch(mobileNumber, REGEX_MOBILENUMBER);
         }
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, REGEX_PASSWORD);
+        }
     }
 }
+
 
